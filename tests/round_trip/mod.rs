7633a70d0ed03c4cc266277ea1e9b99e46adea0b
thealmarty <thealmartyblog@gmail.com>
2024-07-16 10:04:21 -0700

    Correction on endianness.

--- tests/round_trip/mod.rs
+++ tests/round_trip/mod.rs
@@ -236,7 +236,7 @@ fn elf_any() {
         (Architecture::Avr, Endianness::Little),
         (Architecture::Bpf, Endianness::Little),
         (Architecture::Csky, Endianness::Little),
-        (Architecture::Delendum, Endianness::Little),
+        (Architecture::Delendum, Endianness::Big),
         (Architecture::I386, Endianness::Little),
         (Architecture::X86_64, Endianness::Little),
         (Architecture::X86_64_X32, Endianness::Little),
